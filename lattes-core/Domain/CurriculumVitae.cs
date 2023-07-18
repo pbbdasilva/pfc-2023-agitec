7fe2ea9318314474f91a4d94844876f5cd73508e
Pedro Barros <pedro.barros@vtex.com.br>
2023-07-17 22:07:53 -0300

    add more info to Cv

--- lattes-core/Domain/CurriculumVitae.cs
+++ lattes-core/Domain/CurriculumVitae.cs
@@ -9,9 +9,10 @@ public class CurriculumVitae
 	public string? Id { get; set; }
 	public string? Summary { get; set; }
 	public List<Idiom>? Idioms {  get; set; }
-	public List<string>? ActingAreas { get; set; }
-	public List<string>? AdditionalInfoList { get; set; }
-	
+	public Undergrad Undergrad { get; set; }
+	public Masters Masters { get; set; }
+	public Doctorate Doctorate { get; set; }
+
 	public static string GenerateFileName(string id)
 	{
 		return $"cv_{id}.xml";
@@ -33,29 +34,30 @@ public class CurriculumVitae
 			Writing = idiom.PROFICIENCIADEESCRITA.ToString(),
 			Comprehension = idiom.PROFICIENCIADECOMPREENSAO.ToString()
 		}).ToList();
-		// ActingAreas = cv.ATUACOESPROFISSIONAIS.Select(area => area.ATUACAOPROFISSIONAL).ToList();
-		// AdditionalInfoList = cv.DADOSCOMPLEMENTARES.Select(info => info.DADOSCOMPLEMENTARES).ToList();
+
+		Undergrad = new Undergrad
+		{
+			Institution = cv.DADOSGERAIS.FORMACAOACADEMICATITULACAO.GRADUACAO[0].NOMEINSTITUICAO,
+			Course = cv.DADOSGERAIS.FORMACAOACADEMICATITULACAO.GRADUACAO[0].NOMECURSO,
+			ThesisTitle = cv.DADOSGERAIS.FORMACAOACADEMICATITULACAO.GRADUACAO[0].TITULODOTRABALHODECONCLUSAODECURSO,
+		};
+		
+		Masters = new Masters
+		{
+			Institution = cv.DADOSGERAIS.FORMACAOACADEMICATITULACAO.MESTRADO[0].NOMEINSTITUICAO,
+			Course = cv.DADOSGERAIS.FORMACAOACADEMICATITULACAO.MESTRADO[0].NOMECURSO,
+			ThesisTitle = cv.DADOSGERAIS.FORMACAOACADEMICATITULACAO.MESTRADO[0].TITULODADISSERTACAOTESE,
+			Areas = Masters.ParseKnowledgeAreas(cv),
+			Keywords = Masters.ParseKeywords(cv)
+		};
+
+		Doctorate = new Doctorate
+		{
+			Institution = cv.DADOSGERAIS.FORMACAOACADEMICATITULACAO.DOUTORADO[0].NOMEINSTITUICAO,
+			Course = cv.DADOSGERAIS.FORMACAOACADEMICATITULACAO.DOUTORADO[0].NOMECURSO,
+			ThesisTitle = cv.DADOSGERAIS.FORMACAOACADEMICATITULACAO.DOUTORADO[0].TITULODADISSERTACAOTESE,
+			Areas = Doctorate.ParseKnowledgeAreas(cv),
+			Keywords = Doctorate.ParseKeywords(cv)
+		};
 	}
 }
-
-/*
- * dados-complementares
-	
-	formacao complementar
-		outros
-		cursos de curta duracao
-		formacao complementar de extensao universitaria
-		mba
-			nome do curso
-			nome instituicao
-dados gerais
-	formacao academica titulacao
-		tudo menos ensino medio
-	atuacoes profissionais
-		tudo menos vinculos
-	atividades de servico tecnico especializado
-	atividades de estagio
-	atividades de direcao e administracao
-	atividades de participacao em projeto
-		nome�do�projeto
-*/
