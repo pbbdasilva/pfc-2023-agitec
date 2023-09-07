4a106e8906e9bf2fd13e25532257143e7938f508
Pedro <pbbdasilva@gmail.com>
2023-09-07 15:15:37 -0300

    add exception handling when no connection to api and comments

--- lattes-core/store_cvs.py
+++ lattes-core/store_cvs.py
@@ -2,15 +2,27 @@ import requests as req
 from concurrent.futures import ProcessPoolExecutor
 import asyncio
 import os
+import argparse
 
 base_url = "http://localhost:5037/cv/"
 
+'''
+    async calls to lattes-core api
+    the base_url should be set to the endpoint where the api is running
+    if you receive connection refused when calling this method it is probably
+    wrong url or api is not running
 
+    will use cvs in current directory
+'''
 def store_cv(cnpq_id):
     # print("sending put request for {}".format(cnpq_id))
     url = base_url + cnpq_id
-    r = req.get(url)
-    print(r.status_code)
+    try:
+        r = req.get(url)
+    except req.exceptions.ConnectionError as e:
+        print("error for {}".format(cnpq_id))
+        print(e)
+    
     if r.status_code != 200:
         print("error for {}".format(cnpq_id))
 
@@ -25,4 +37,13 @@ async def main():
     
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Store resumes from current directory to mongodb')
+    parser.add_argument('--url', help='API endpoint')
+    args = parser.parse_args()
+    url = args.url
+
+    # if no url is defined, we assume the correct one is already in the script
+    if url:
+        base_url = url
+
     asyncio.run(main())
