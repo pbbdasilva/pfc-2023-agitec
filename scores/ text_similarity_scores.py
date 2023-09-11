ee1067fbc8c3baf514a7e133c3784752ee4f2801
andreluizfv <andreluiz.lf11@hotmail.com>
2023-09-11 20:14:04 -0300

    all similarities function dones, and renamed

--- scores/ text_similarity_scores.py
+++ scores/ text_similarity_scores.py
@@ -13,7 +13,7 @@ URI = f'''mongodb+srv://{username}:{password}@lattes-pfc-2023.twn2hk2.mongodb.ne
 
 client = MongoClient(URI,server_api = ServerApi('1'))
 
-class Undergrad:
+class Education:
     def __init__(self, institution: str, course: str, thesisTitle: str):
         self.institution = institution
         self.course = course
@@ -21,60 +21,65 @@ class Undergrad:
 
     def __str__(self):
         return self.course + " " + self.thesisTitle
-
-class Masters:
+    
+class HighLevelEducation(Education):
     def __init__(self, master: dict):
-        self.institution = master['Institution']
-        self.course = master['Course']
+        super.__init__(master)
         self.thesisTitle = master['ThesisTitle']
         self.areas = " ".join(master['Areas'])
         self.keywords = " ".join(master['Keywords'])
-
     def __str__(self):
         return self.course + " " + self.thesisTitle + " " + self.areas + " " + self.keywords
-    
-class Doctorate:
+
+class Masters(Education):
     def __init__(self, master: dict):
-        self.institution = master['Institution']
-        self.course = master['Course']
-        self.thesisTitle = master['ThesisTitle']
-        self.areas = " ".join(master['Areas'])
-        self.keywords = " ".join(master['Keywords'])
+        super.__init__(master)
 
     def __str__(self):
-        return self.course + " " + self.thesisTitle + " " + self.areas + " " + self.keywords 
-
-class WorkExp:
-    def __init__(self, workexpDict: dict):
-        self.institution = workexpDict['Institution']
-        self.knowledgeArea = workexpDict['KnowledgeArea']
+        return super.__str__()
+    
+class Doctorate(Education):
+    def __init__(self, master: dict):
+        super.__init__(master)
 
     def __str__(self):
-        return self.course + " " + self.thesisTitle + " " + self.areas + " " + self.keywords 
+        return super.__str__()
 
-def get_summary_score(cv: dict, keywords: list) -> float:
+def get_summary_similarity(cv: dict, keywords: list) -> float:
     return compare([cv['summary']], keywords)[0]
 
-def get_undergrad_scores(cv: dict, keywords: list) -> list:
-    return compare([str(Undergrad(u['Institution'], u['Course'], u['ThesisTitle'])) for u in  cv['undergrad']], keywords)
+def get_undergrad_similarities(cv: dict, keywords: list) -> list:
+    return compare([str(Education(u['Institution'], u['Course'], u['ThesisTitle'])) for u in  cv['undergrad']], keywords)
 
-def get_posgrad_score(cv: dict, keywords: list) -> float:
+def get_posgrad_similarity(cv: dict, keywords: list) -> float:
     return compare([cv['posgrad']['Course'] + " " + cv['posgrad']['ThesisTitle'] ], keywords)[0]
 
-def get_masters_score(cv: dict, keywords: list) -> float:
+def get_masters_similarity(cv: dict, keywords: list) -> float:
     return compare([str(Masters(cv['masters']))], keywords)[0]
 
-def get_doctorate_score(cv: dict, keywords: list) -> float:
+def get_doctorate_similarity(cv: dict, keywords: list) -> float:
     return compare([str(Doctorate(cv['doctorate']))], keywords)[0]
 
-def get_workexp_scores(cv: dict, keywords: list) -> list:
-    return compare([str(WorkExp(workexpDict)) for workexpDict in  cv['workexp']], keywords)
+# def get_workexp_similarities(cv: dict, keywords: list) -> list: 
+#     return compare([str(WorkExp(workexpDict)) for workexpDict in  cv['workexp']], keywords)
+
+def get_areasList_similarities(cv: dict, keywords: list) -> list: 
+    return compare([areaDict['Specialty'] + " " + areaDict['KnowledgeArea'] for areaDict in  cv['areas']], keywords)
+
+def get_articles_similarities(cv: dict, keywords: list) -> list: 
+    return compare([article for article in cv['articles']], keywords)
 
 database = client["lattes"]
 collection = database["resumes"]
-documents = collection.find({"_id" : "4904387541295597"})
+documents = collection.find({"_id" : "5431165627826233"})
 for document in documents:
-    print(str(Doctorate(document['doctorate'])))
-    print(get_masters_score(document, ["Computação", "Segurança", "Leis", "Legislação", "Redigir"]))
+    print(document['articles'])
+    print(get_articles_similarities(document, ["Computação", "Segurança", "Leis", "Legislação", "Redigir"]))
+    print(get_undergrad_similarities(document, ["Computação", "Segurança", "Leis", "Legislação", "Redigir"]))
+    print(get_areasList_similarities(document, ["Computação", "Segurança", "Leis", "Legislação", "Redigir"]))
+    print(get_doctorate_similarity(document, ["Computação", "Segurança", "Leis", "Legislação", "Redigir"]))
+    print(get_posgrad_similarity(document, ["Computação", "Segurança", "Leis", "Legislação", "Redigir"]))
+    print(get_masters_similarity(document, ["Computação", "Segurança", "Leis", "Legislação", "Redigir"]))
+
     
 client.close()
