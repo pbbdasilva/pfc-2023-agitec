ccff0411be016cae49344d2fdb3cf4c5fac69369
Bernard Reznik <bernard@172-15-6-174.lightspeed.stlsmo.sbcglobal.net>
2023-09-11 11:55:16 -0300

    esqueleto da main alterado com as funções

--- main.py
+++ main.py
@@ -19,3 +19,10 @@ def get_score_similaridade_textual(nce: json, candidato:pd.Series) -> float:
 
 def get_score_candidato(nce: json) -> pd.DataFrame:
     pass
+
+def main(cod_NCE: string) -> pd.DataFrame:
+    nce = get_NCE(cod_NCE)
+    candidatos = get_universo_candidatos(nce)
+    candidatos['score_geral'] = candidatos.apply(lambda x: get_score_geral(nce, x), axis=1)
+    candidatos['score_similaridade_textual'] = candidatos.apply(lambda x: get_score_similaridade_textual(nce, x), axis=1)
+    candidatos['score_candidato'] = candidatos['score_geral'] + candidatos['score_similaridade_textual']
