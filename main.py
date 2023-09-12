2ba5dbf3e64ef76eb5100ee38af35d57f60fc84d
andreluizfv <andreluiz.lf11@hotmail.com>
2023-09-12 13:57:43 -0300

    get_universo_candidates done

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ from scores import general_scores
 from nce_etl import nce_utils
 
 
+def get_universo_candidatos(nce: json, all_candidates: pd.DataFrame) -> list:
+    # nce['patente'] = asdasd
+    targetRanks = nu.translate_posto_nce_to_portal_da_transparencia(nu.get_requisito_posto_nce(nce))
+    filtered = [all_candidates[
+            (~all_candidates['ORG_LOTACAO'].isin(targetRanks)) &
+            (all_candidates['DESCRICAO_CARGO'] == targetRank)]['NOME'] 
+                 for targetRank in targetRanks]
+    names = filtered['NOME'].values.tolist()
+    # ids = webscraper.getNameIdsDict(names)
+    # for id in ids['id']:
+    #   download(id)
+    #   upToMongo(id)
+    return collection.find(  {"author" : { "$in": names} })
 
 def get_universo_candidatos(nce: json, all_candidates: pd.DataFrame) -> pd.DataFrame:
     pass
