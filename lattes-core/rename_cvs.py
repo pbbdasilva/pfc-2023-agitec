3b4d964cc9da5662c32927e222f62c8702b0f9d1
Pedro <pbbdasilva@gmail.com>
2023-09-14 22:55:34 -0300

    start adding rank to cv file

--- lattes-core/rename_cvs.py
+++ lattes-core/rename_cvs.py
@@ -1,35 +1,53 @@
+import argparse
 import zipfile
 import os
 
+TMP_DIR = "tmp/"
+
 '''
 after we unzip the file, they all have the same name (curriculo.xml)
-so we need to rename them to our format -> cv_{cnpq_id}.xml
+so we need to rename them to our format -> cv_{rank}_{cnpq_id}.xml
 '''
-def rename_cv(cnpq_id):
-    default_file_name = "curriculo.xml"
-    new_file_name = "cv_{}.xml".format(cnpq_id)
+def rename_cv(cnpq_id, rank):
+    default_file_name = TMP_DIR + "curriculo.xml"
+    new_file_name = "cv_{}_{}.xml".format(rank, cnpq_id)
     os.rename(default_file_name, new_file_name)
 
 '''
 cvs come zipped from lattes
 '''
-def unzip():
+def unzip(rank):
     files = os.listdir(os.getcwd())
-    cnpq_zips = [file for file in files if file.endswith('zip')]
+    # get zip only for the given rank
+    cnpq_zips = [file for file in files if file.endswith('zip') and file.startswith(rank)]
 
-    for zipped_cv in cnpq_zips:
-        with zipfile.ZipFile(zipped_cv,"r") as zip_ref:
-            zip_ref.extractall()
-        
-        cnpq_id = zipped_cv.replace(".zip", "")
-        rename_cv(cnpq_id)
+    if len(cnpq_zips) == 0:
+        print("No files for target rank were found")
+        exit(1)
+
+    for zipped_rank in cnpq_zips:
+        with zipfile.ZipFile(zipped_rank,"r") as zip_ref:
+            zip_ref.extractall(TMP_DIR)
+        zipped_cvs = [file for file in os.listdir(os.getcwd() + "/" + TMP_DIR) if file.endswith(".zip")]
+        for zipped_cv in zipped_cvs:
+            with zipfile.ZipFile(TMP_DIR + zipped_cv, "r") as zip_ref:
+                zip_ref.extractall(TMP_DIR)
+                rename_cv(zipped_cv.replace(".zip", ""), rank)
     
-    # after we rename it, we can delete de zipped file
-    for zipped_cv in cnpq_zips:
-        os.remove(zipped_cv)
-
-def main():
-    unzip()
+    # after we rename it, we can delete the zip files
+    for zipped_rank in cnpq_zips:
+        os.remove(zipped_rank)
+    for zipped_cv in os.listdir(TMP_DIR):
+        os.remove(TMP_DIR + zipped_cv)
+    os.rmdir(TMP_DIR)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Store resumes from current directory to mongodb')
+    parser.add_argument('--rank', help='Rank parameter')
+    args = parser.parse_args()
+    rank = args.rank
+
+    if not rank:
+        print("Error: Rank parameter (--rank) is required.")
+        exit(1)
+    unzip(rank)
