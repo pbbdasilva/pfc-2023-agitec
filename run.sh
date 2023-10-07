28fe1a62100992eeff10fc91bb502014521072b3
Pedro <pbbdasilva@gmail.com>
2023-10-07 17:07:16 -0300

    fix setup

--- run.sh
+++ run.sh
@@ -2,19 +2,13 @@
 PFC_DIR=/home/utilizador/pfc-2023-agitec
 $PFC_DIR/run-dotnet.sh
 
-# fetch and filter candidates step
-cd $PFC_DIR/candidates
 source $PFC_DIR/venv/bin/activate
-python3 fetch_candidates.py --month 01 --year 2023
-python3 fetch_ids.py
 
-# store candidates step
-cd $PFC_DIR/lattes-core
-python3 store_cvs.py
-
-cd $PFC_DIR/nce
-python3 parse_nces.py
-python3 store_nces.py
-
-cd $PFC_DIR/scores
-python3 main.py
+cd $PFC_DIR/candidates/
+./run.sh
+cd $PFC_DIR/lattes-core/
+./run.sh
+cd $PFC_DIR/nce/
+./run.sh
+cd $PFC_DIR/scores/
+./run.sh
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 sudo apt-get update
 sudo apt-get install -y dotnet-sdk-7.0
 sudo apt-get install -y dotnet-runtime-7.0
+pip3 install -r requirements.txt
